ffa2b4fefe787beacd86ae94acb10d6bc14d6a54
Sushant <Sush@Sush-PC>
2018-01-31 16:35:40 +0530

    First git project

---
+++ TEsting/src/com/webdriver/Testcase1.java
@@ -0,0 +1,43 @@
+package com.webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Testcase1 {
+
+	public static void main(String[] args) {
+		
+		
+		//System.setProperty("webdriver.firefox.marionette","D:\\geckodriver.exe");
+		//WebDriver driver = new FirefoxDriver();
+		//comment the above 2 lines and uncomment below 2 lines to use Chrome
+		System.setProperty("webdriver.chrome.driver","D:\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+    	
+        String baseUrl = "http://demo.guru99.com/selenium/newtours/";
+        String expectedTitle = "Welcome: Mercury Tours";
+        String actualTitle = "";
+
+        // launch Fire fox and direct it to the Base URL
+        driver.get(baseUrl);
+
+        // get the actual value of the title
+        actualTitle = driver.getTitle();
+
+        /*
+         * compare the actual title of the page with the expected one and print
+         * the result as "Passed" or "Failed"
+         */
+        if (actualTitle.contentEquals(expectedTitle)){
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed");
+        }
+       
+        //close Fire fox
+        driver.close();
+
+	}
+
+}
