7671696e5fa140498201a85a197a298d1bec9064
Sushant <spatil@daxko.com>
2018-02-20 16:04:16 +0530

    window handle

--- TEsting/src/com/webdriver/TestCase2.java
+++ TEsting/src/com/webdriver/TestCase2.java
@@ -1,9 +1,14 @@
 package com.webdriver;
 
+import java.util.Set;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
@@ -20,25 +25,44 @@ public class TestCase2 {
 			driver.get("https://www.google.co.in");
 		}
 		
-	    @Test
-	    @DataProvider
+	    @Test(dataProvider="searchs")
 		public void searchs(String search_text) throws InterruptedException
 		{
+	    	
+	    	String mainWin=driver.getWindowHandle();
 			WebElement search_box= driver.findElement(By.xpath("//*[@name='q']"));
 			search_box.sendKeys(search_text);
-			Thread.sleep(10000);
-			driver.findElement(By.xpath("//*h3[class='r']"));
-			Thread.sleep(10000);
+			Thread.sleep(2000);
+			driver.findElement(By.xpath("//input[@value='Google Search']")).click();
+			Thread.sleep(2000);
+			Actions builder=new Actions(driver);
+			Action a= builder.keyDown(Keys.SHIFT).build();
+			a.perform();
+			driver.findElement(By.linkText(search_text)).click();
+			Thread.sleep(2000);
+			Action b=builder.keyUp(Keys.SHIFT).build();
+			b.perform();
+			driver.switchTo().window(mainWin);
+			driver.navigate().back();
+//			System.out.println("here");
+//			Set<String> area=driver.getWindowHandles();
+		//	driver.switchTo().window(area.)
 			
 			//driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS );
 		
 		}
 
 	    @AfterTest
-	    public void Finish()
+	    public void Finish() throws InterruptedException
 		{
 		
-	    	driver.close();
+	    	Set<String> area=driver.getWindowHandles();
+	    	for(String a : area)
+	    	{
+	    		System.out.println( "Value"+a); 
+	    	    driver.switchTo().window(a);
+	    	    Thread.sleep(2000);
+	    	}	
 	    	
 	    	
 		}
@@ -47,7 +71,7 @@ public class TestCase2 {
 	    public Object[] lookps()
 	    {
 	    	
-	    	Object search_text[]={"Android","IOS","Blackberry"};
+	    	Object search_text[]={"Android","iPhone - Apple (IN)","Nokia Corporation","Gmail"};
 	    	
 			return search_text;
 	    	
